6e6e5d9d7f9439f371c6f227ed468c5acef24c0d
Hp <adkhamjon.abd@gmail.com>
2023-04-09 07:22:43 +0200

    Corrects some mistakes

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,15 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 'c';
-        int h = 'h';
-        int a = 'a';
-        int r = 'r';
-
-        c = (int) c;
-        h = (int) h;
-        a = (int) a;
-        r = (int) r;
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
 
         System.out.println(c);
         System.out.println(h);
